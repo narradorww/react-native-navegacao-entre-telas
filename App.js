36a292f26b607fbee627e4bc0581c70821705546
narradorww <rodrigo.anst@gmail.com>
2023-01-18 23:14:43 -0300

    Implementado o arquivo de rotas

--- App.js
+++ App.js
@@ -1,26 +1,12 @@
 import React from 'react';
 import {StatusBar, SafeAreaView} from 'react-native';
-import useProdutores from './src/hooks/useProdutores';
+import AppRotas from './src/rotas/AppRotas';
 
-import Home from './src/telas/Home';
-import MelhoresProdutores from './src/telas/MelhoresProdutores';
-
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-
-const Tab = createBottomTabNavigator();
 export default function App() {
-  
-
   return (
     <SafeAreaView style={{flex: 1}}>
       <StatusBar />
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={Home} />
-          <Tab.Screen name="Melhores Produtores" component={MelhoresProdutores} />
-        </Tab.Navigator>
-      </NavigationContainer>
+      <AppRotas />
     </SafeAreaView>
   );
 }
